c2cab79bdc3ee53ea59f914f4334e714598248c1
Jared <jaredl3106@gmail.com>
2024-10-15 21:24:19 -0500

    Initial page layout and website structure

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <header className="bg-gray-800 text-white py-4">
+      <h1 className="text-center text-3xl">Workout Tracker</h1>
+    </header>
+  );
+};
+
+export default Header;
